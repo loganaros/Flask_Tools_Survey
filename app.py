0e799d99ccaf4d437d84892654f018bf4c4b4e23
loganaros <logabeast11@gmail.com>
2024-02-05 11:19:45 -0700

    modified to use session

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, flash
+from flask import Flask, request, render_template, redirect, flash, session
 from surveys import satisfaction_survey
 from flask_debugtoolbar import DebugToolbarExtension
 
@@ -8,33 +8,38 @@ app.config['SECRET_KEY'] = 'ajeifjaifjaeofjd'
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 # debug = DebugToolbarExtension(app)
 
-responses = []
-
 @app.route("/")
 def survey_page():
     return render_template("home.html",  title=satisfaction_survey.title, instructions=satisfaction_survey.instructions, questions=satisfaction_survey.questions)
 
 @app.route("/start", methods=["POST"])
 def start_survey():
+    session['responses'] = []
     return redirect("/questions/0")
 
 @app.route("/thank-you")
 def end_page():
-    return render_template("thank-you.html", responses=responses)
+    return render_template("thank-you.html", responses=session.get('responses'))
 
 @app.route("/questions/<int:question_number>")
 def questions_page(question_number):
-    if len(responses) != question_number:
+    if len(session.get('responses')) == len(satisfaction_survey.questions):
+        return redirect('/thank-you')
+
+    if len(session.get('responses')) != question_number:
         flash("Invalid Response")
-        return redirect(f"/questions/{len(responses)}")
+        # raise
+        return redirect(f"/questions/{len(session.get('responses'))}")
     
     return render_template("questions.html", title=satisfaction_survey.title, instructions=satisfaction_survey.instructions, question=satisfaction_survey.questions[question_number])
 
 @app.route("/answer", methods=["POST"])
 def save_answer():
+    responses = session['responses']
     responses.append(request.form["choice"])
+    session['responses'] = responses
 
-    if len(responses) < len(satisfaction_survey.questions):
-        return redirect(f"/questions/{len(responses)}")
+    if len(session.get('responses')) < len(satisfaction_survey.questions):
+        return redirect(f"/questions/{len(session.get('responses'))}")
     else:
         return redirect("/thank-you")
